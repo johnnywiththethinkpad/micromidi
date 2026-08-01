c2687bf5f913578e242e4aa501b186b45155ef93
johnnywiththethinkpad <859366@pdsb.net>
2026-07-31 21:53:25 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def on_button_pressed_ab():
-    Cursor.delete()
+    stop_scan()
     basic.show_leds("""
         # # # # #
         . . . . .
@@ -42,56 +42,141 @@ def on_button_pressed_ab():
     control.reset()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
+
 def on_logo_pressed():
-    if Cursor.get(LedSpriteProperty.Y) == 4:
-        music.play(music.create_sound_expression(WaveShape.SQUARE,
-                200,
-                1,
-                255,
-                0,
-                100,
-                SoundExpressionEffect.NONE,
-                InterpolationCurve.CURVE),
-            music.PlaybackMode.UNTIL_DONE)
-    else:
-        music.play(music.tone_playable(WhatNote, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
+    global cursor_x, cursor_y
+    if not scan_active:
+        note_grid[cursor_x][cursor_y] = not note_grid[cursor_x][cursor_y]
+        render_scene()
+        if cursor_y == 4:
+            music.play(music.create_sound_expression(WaveShape.SQUARE,
+                    220,
+                    1,
+                    180,
+                    0,
+                    40,
+                    SoundExpressionEffect.NONE,
+                    InterpolationCurve.CURVE),
+                music.PlaybackMode.UNTIL_DONE)
+        else:
+            music.play(music.tone_playable(WhatNote, music.beat(BeatFraction.WHOLE)),
+                music.PlaybackMode.UNTIL_DONE)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
+
+def on_button_pressed_a():
+    stop_scan()
+
+
+def on_button_pressed_b():
+    stop_scan()
+
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+
+def on_gesture_shake():
+    global scan_active, scan_column
+    scan_active = True
+    scan_column = 0
+    render_scene()
+
+
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
 WhatNote = 0
-Cursor: game.LedSprite = None
-Cursor = game.create_sprite(4, 0)
+cursor_x = 4
+cursor_y = 0
+scan_active = False
+scan_column = 0
+note_grid = [[False for _ in range(5)] for _ in range(5)]
+blink_state = False
+blink_timer = 0
+
+
+def render_scene():
+    basic.clear_screen()
+    for x in range(5):
+        for y in range(5):
+            is_cursor_cell = x == cursor_x and y == cursor_y
+            cursor_on_note = note_grid[cursor_x][cursor_y]
+            should_hide_cursor = is_cursor_cell and cursor_on_note and not blink_state
+            if note_grid[x][y] and not should_hide_cursor:
+                led.plot(x, y)
+    if scan_active:
+        for y in range(5):
+            if not (scan_column == cursor_x and y == cursor_y and not blink_state):
+                led.plot(scan_column, y)
+    if not scan_active:
+        if note_grid[cursor_x][cursor_y]:
+            if blink_state:
+                led.plot(cursor_x, cursor_y)
+        else:
+            led.plot(cursor_x, cursor_y)
+
+
+def stop_scan():
+    global scan_active
+    scan_active = False
+    render_scene()
+
+
+def set_what_note():
+    global WhatNote
+    if cursor_y == 0:
+        WhatNote = 262
+    elif cursor_y == 1:
+        WhatNote = 311
+    elif cursor_y == 2:
+        WhatNote = 349
+    elif cursor_y == 3:
+        WhatNote = 392
+    else:
+        WhatNote = 0
+
+
+set_what_note()
+render_scene()
+
 
 def on_forever():
-    if Cursor.get(LedSpriteProperty.X) == 4 and input.button_is_pressed(Button.B):
+    global cursor_x, cursor_y
+    if not scan_active and input.button_is_pressed(Button.B):
         basic.pause(150)
-        Cursor.set(LedSpriteProperty.X, 0)
-    else:
-        if input.button_is_pressed(Button.B):
-            basic.pause(150)
-            Cursor.change(LedSpriteProperty.X, 1)
+        cursor_x = (cursor_x + 1) % 5
+        set_what_note()
+        render_scene()
 basic.forever(on_forever)
 
+
 def on_forever2():
-    global WhatNote
-    if Cursor.get(LedSpriteProperty.Y) == 0:
-        WhatNote = 262
-    if Cursor.get(LedSpriteProperty.Y) == 1:
-        WhatNote = 311
-    if Cursor.get(LedSpriteProperty.Y) == 2:
-        WhatNote = 349
-    if Cursor.get(LedSpriteProperty.Y) == 3:
-        WhatNote = 392
-    if Cursor.get(LedSpriteProperty.Y) == 4:
-        WhatNote = 0
+    global cursor_y
+    if not scan_active and input.button_is_pressed(Button.A):
+        basic.pause(150)
+        cursor_y = (cursor_y + 1) % 5
+        set_what_note()
+        render_scene()
 basic.forever(on_forever2)
 
+
 def on_forever3():
-    if Cursor.get(LedSpriteProperty.Y) == 4 and input.button_is_pressed(Button.A):
-        basic.pause(150)
-        Cursor.set(LedSpriteProperty.Y, 0)
-    else:
-        if input.button_is_pressed(Button.A):
-            basic.pause(150)
-            Cursor.change(LedSpriteProperty.Y, 1)
+    global scan_active, scan_column
+    if scan_active:
+        render_scene()
+        basic.pause(50)
+        scan_column = (scan_column + 1) % 5
+        if input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B):
+            stop_scan()
 basic.forever(on_forever3)
+
+
+def on_forever4():
+    global blink_state, blink_timer
+    blink_timer += 1
+    if blink_timer >= 0:
+        blink_timer = 0
+        blink_state = not blink_state
+        render_scene()
+    basic.pause(1)
+basic.forever(on_forever4)
--- main.ts
+++ main.ts
@@ -1,61 +1,185 @@
-input.onButtonPressed(Button.AB, function () {
-    Cursor.delete()
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    stop_scan()
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
     control.reset()
 })
-input.onGesture(Gesture.Shake, function () {
-    gottasweepsweepsweep = game.createSprite(4, 0)
-    Cursor.set(LedSpriteProperty.X, 50)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (Cursor.get(LedSpriteProperty.Y) == 4) {
-        music.play(music.createSoundExpression(WaveShape.Square, 200, 1, 255, 0, 100, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
-    } else {
-        music.play(music.tonePlayable(WhatNote, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    
+    if (!scan_active) {
+        note_grid[cursor_x][cursor_y] = !note_grid[cursor_x][cursor_y]
+        render_scene()
+        if (cursor_y == 4) {
+            music.play(music.createSoundExpression(WaveShape.Square, 220, 1, 180, 0, 40, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
+        } else {
+            music.play(music.tonePlayable(WhatNote, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        }
+        
     }
-    Note2 = game.createSprite(Cursor.get(LedSpriteProperty.X), Cursor.get(LedSpriteProperty.Y))
+    
 })
-let Note2: game.LedSprite = null
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    stop_scan()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    stop_scan()
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    scan_active = true
+    scan_column = 0
+    render_scene()
+})
 let WhatNote = 0
-let gottasweepsweepsweep: game.LedSprite = null
-let Cursor: game.LedSprite = null
-Cursor = game.createSprite(4, 0)
-basic.forever(function () {
-    basic.pause(150)
-    if (Cursor.get(LedSpriteProperty.Y) == 4 && input.buttonIsPressed(Button.A)) {
-        basic.pause(150)
-        Cursor.set(LedSpriteProperty.Y, 0)
-    } else {
-        if (input.buttonIsPressed(Button.A)) {
-            basic.pause(150)
-            Cursor.change(LedSpriteProperty.Y, 1)
+let cursor_x = 4
+let cursor_y = 0
+let scan_active = false
+let scan_column = 0
+let note_grid =  {TODO: ListComp} 
+let blink_state = false
+let blink_timer = 0
+function render_scene() {
+    let y: number;
+    let is_cursor_cell: any;
+    let cursor_on_note: any;
+    let should_hide_cursor: any;
+    basic.clearScreen()
+    for (let x = 0; x < 5; x++) {
+        for (y = 0; y < 5; y++) {
+            is_cursor_cell = x == cursor_x && y == cursor_y
+            cursor_on_note = note_grid[cursor_x][cursor_y]
+            should_hide_cursor = is_cursor_cell && cursor_on_note && !blink_state
+            if (note_grid[x][y] && !should_hide_cursor) {
+                led.plot(x, y)
+            }
+            
         }
     }
-})
-basic.forever(function () {
-    if (Cursor.get(LedSpriteProperty.X) == 4 && input.buttonIsPressed(Button.B)) {
-        basic.pause(150)
-        Cursor.set(LedSpriteProperty.X, 0)
-    } else {
-        if (input.buttonIsPressed(Button.B)) {
-            basic.pause(150)
-            Cursor.change(LedSpriteProperty.X, 1)
+    if (scan_active) {
+        for (y = 0; y < 5; y++) {
+            if (!(scan_column == cursor_x && y == cursor_y && !blink_state)) {
+                led.plot(scan_column, y)
+            }
+            
         }
     }
-})
-basic.forever(function () {
-    if (Cursor.get(LedSpriteProperty.Y) == 0) {
+    
+    if (!scan_active) {
+        if (note_grid[cursor_x][cursor_y]) {
+            if (blink_state) {
+                led.plot(cursor_x, cursor_y)
+            }
+            
+        } else {
+            led.plot(cursor_x, cursor_y)
+        }
+        
+    }
+    
+}
+
+function stop_scan() {
+    
+    scan_active = false
+    render_scene()
+}
+
+function set_what_note() {
+    
+    if (cursor_y == 0) {
         WhatNote = 262
-    }
-    if (Cursor.get(LedSpriteProperty.Y) == 1) {
+    } else if (cursor_y == 1) {
         WhatNote = 311
-    }
-    if (Cursor.get(LedSpriteProperty.Y) == 2) {
+    } else if (cursor_y == 2) {
         WhatNote = 349
-    }
-    if (Cursor.get(LedSpriteProperty.Y) == 3) {
+    } else if (cursor_y == 3) {
         WhatNote = 392
-    }
-    if (Cursor.get(LedSpriteProperty.Y) == 4) {
+    } else {
         WhatNote = 0
     }
+    
+}
+
+set_what_note()
+render_scene()
+basic.forever(function on_forever() {
+    
+    if (!scan_active && input.buttonIsPressed(Button.B)) {
+        basic.pause(150)
+        cursor_x = (cursor_x + 1) % 5
+        set_what_note()
+        render_scene()
+    }
+    
+})
+basic.forever(function on_forever2() {
+    
+    if (!scan_active && input.buttonIsPressed(Button.A)) {
+        basic.pause(150)
+        cursor_y = (cursor_y + 1) % 5
+        set_what_note()
+        render_scene()
+    }
+    
+})
+basic.forever(function on_forever3() {
+    
+    if (scan_active) {
+        render_scene()
+        basic.pause(50)
+        scan_column = (scan_column + 1) % 5
+        if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
+            stop_scan()
+        }
+        
+    }
+    
+})
+basic.forever(function on_forever4() {
+    
+    blink_timer += 1
+    if (blink_timer >= 0) {
+        blink_timer = 0
+        blink_state = !blink_state
+        render_scene()
+    }
+    
+    basic.pause(1)
 })
